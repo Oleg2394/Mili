99be2d3d34f3aad4ddfa40280c255d69d9909cb3
Oleg <maczak94@yandex.ru>
2020-05-04 07:36:06 +0700

    Add src/

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket = 17000; // цена билета
+        int bonus = ticket / 20;
+        System.out.println("Количество бонусов = " + bonus);
+    }
+}
